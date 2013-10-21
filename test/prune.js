fbd0ef27c1b71471bac3183135e79923cf03db72
John Madhavan-Reese <jsmreese@pureshare.com>
2013-10-21 11:55:27 -0400

    Added prune method.

---
+++ test/prune.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+	// tests
+	module('Prune');
+
+	test("remove properties with empty string values", function () {
+		deepEqual(_.prune({
+			a: "",
+			b: 123,
+			c: 0,
+			d: null,
+			e: undefined,
+			f: NaN,
+			g: true,
+			h: false,
+			i: {},
+			j: [],
+			k: "string",
+			l: "",
+			m: {
+				m1: "",
+				m2: ""
+			}
+		}), {
+			b: 123,
+			c: 0,
+			d: null,
+			e: undefined,
+			f: NaN,
+			g: true,
+			h: false,
+			i: {},
+			j: [],
+			k: "string",
+			m: {
+				m1: "",
+				m2: ""
+			}
+		});
+	});
+});
